27d695557eb0105e510af22683b064f0daea52de
UITAEK LEE <dtmaster@empal.com>
2019-07-09 10:06:08 +0900

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-import UserInput from './userinput'
+import UserInput from './UserInput'
+import SalesLists from './Table'
 
 
 function App() {
   return (
+    
+    
     <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
       <h1>Sales Management System</h1>
       <UserInput/>
+      <SalesLists/>
     </div>
            
   );
